481b78d16723fbd87572d4af2e19a93974917dbd
Anh Minh Tran <anhminhtran235@gmail.com>
2020-06-29 07:23:14 -0700

    Delete BellmanFord

--- Graph/test/weighted/testBellmanFord.java
+++ Graph/test/weighted/testBellmanFord.java
@@ -1,15 +1,15 @@
 package Graph.test.weighted;
 
-import Graph.main.weighted.BellmanFordIterative;
-import Graph.main.weighted.BellmanFordRecursive;
+// import Graph.main.weighted.BellmanFordIterative;
+// import Graph.main.weighted.BellmanFordRecursive;
 import Graph.main.weighted.Graph;
 
 public class testBellmanFord {
     public static void main(String[] args) {
-        int numVertices = 35;
-        int numEdges = 800;
+        /*int numVertices = 5;
+        int numEdges = 8;
         boolean isDirected = true;
-        boolean allowNegativeWeight = false;
+        boolean allowNegativeWeight = true;
         Graph g = Graph.generateRandomGraph(numVertices, numEdges, isDirected, allowNegativeWeight);
 
         System.out.println("Graph " + (isDirected ? "(directed):" : "(undirected):"));
@@ -21,6 +21,6 @@ public class testBellmanFord {
         BellmanFordIterative.printShortestPaths(g, 0);
         System.out.println();
         System.out.print("Iterative BellmanFord (optimize space) - ");
-        BellmanFordIterative.printShortestPaths(g, 0);
+        BellmanFordIterative.printShortestPathsBetter(g, 0);*/
     }
 }
